e2ac27ac05ef818fdb6da8cb670ae3e02c19ff9c
anderrester <anderrester@mail.ru>
2022-02-02 13:04:52 +0200

    Structure and a few tasks

--- JavaScript/tasks.js
+++ JavaScript/tasks.js
@@ -1,2 +1,42 @@
+// First task
 console.log("Hello, world!");
-alert("Hello, world!");
+
+// Third task
+'use strict';
+
+// Fourth task
+let userName = "Student";
+console.log(userName);
+// You should never proceed like this!
+const adminName = "Admin";
+console.log(adminName);
+
+// Fifth task
+let whaaaat;
+let truth = true;
+let number = 15;
+let line = "something";
+let objectExample = {};
+
+console.log(whaaaat);
+console.log(truth);
+console.log(number);
+console.log(line);
+console.log(objectExample);
+
+// Sixth task
+
+//Small easter egg a saw films
+alert("I wanna play a game...");
+let answer = prompt("Unfortunately you have only 2 hours to death ", "Nooooo!");
+answer == "Nooooo!" ? alert("If you don't locate the key in time, the mask will close") : alert("So listen carefully...");
+confirm("Well, you'll die anyway...");
+
+// Seventh task
+console.log(typeof whaaaat);
+console.log(typeof truth);
+console.log(typeof number);
+console.log(typeof line);
+console.log(typeof objectExample);
+
+// Eighth task
