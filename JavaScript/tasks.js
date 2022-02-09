d3a335f81ac8053c3fbfae61c09a245a22524c66
AnderRester <AnderRester@icloud.com>
2022-02-09 10:43:46 +0200

    добавление функций и фиксы

--- JavaScript/tasks.js
+++ JavaScript/tasks.js
@@ -77,3 +77,121 @@ switch (enteredNumber) {
         alert("Out of barrier | Выход за границы условия");
         break;
 }
+
+const a = 5;
+const b = 8;
+const c = 2;
+
+alert(((a > b) && (b > c)) ? "Макс - a, Мин - c" : ((a > c) && (c > b)) ? "Макс - a, Мин - b" : ((b > a) && (a > c)) ? "Макс - b, Мин - c" : ((b > c) && (c > a)) ? "Макс - b, Мин - a" : ((c > a) && (a > b)) ? "Макс - c, Мин - b" : ((c > b) && (b > a)) ? "Макс - c, Мин - a" : 1);
+
+//alert("Мин - " + Math.min(a, b, c) + " Макс - " + Math.max(a, b, c));
+
+const nickname = prompt("Введите имя", "Некто");
+
+const ucFirst = (nickname) => {
+    let word = "";
+    for (let i = 1; i <= nickname.length; i++) {
+        word += nickname.charAt(i);
+    }
+    return (nickname.charAt(0).toUpperCase() + word);
+
+}
+alert(ucFirst(nickname));
+
+const CurrencyValue = prompt("Введите тип валюты и размер транзакции", "$5");
+
+const extractCurrencyValue = (CurrencyValue) => {
+    let num = "";
+    for (let i = 1; i <= CurrencyValue.length; i++) {
+        num += CurrencyValue.charAt(i);
+    }
+    return (num);
+}
+alert(extractCurrencyValue(CurrencyValue));
+
+const a = Number(prompt("Введите число в диапазоне от -1 до 1, включая 0", 0));
+
+if (a < 0) {
+    alert("Меньше нуля");
+} else {
+    if (a > 0) {
+        alert("Больше нуля");
+    } else {
+        alert("Равно нулю");
+    }
+}
+
+
+const min = 14;
+const max = 90;
+
+const age = Number(prompt(`Введите возраст в диапазоне от ${min} до ${max}`, 14));
+
+const check = () => {
+    let option = document.getElementById("variant").value;
+    switch (Number(option)) {
+        case 1:
+            if (!(age < 14) && !(age > 90)) {
+                alert("Число принадлежит интервалу");
+            } else {
+                alert("Число не принадлежит интервалу");
+            }
+            break;
+        case 2:
+            if (age >= 14 && age <= 90) {
+                alert("Число принадлежит интервалу");
+            } else {
+                alert("Число не принадлежит интервалу");
+            }
+            break;
+    }
+}
+
+// //C оператором НЕ
+
+// if (!(age <= 14) && !(age >= 90)) {
+//     alert("Число принадлежит интервалу");
+// } else {
+//     alert("Число не принадлежит интервалу");
+// }
+
+// //Без оператора НЕ
+
+// if (age >= 14 && age <= 90) {
+//     alert("Число принадлежит интервалу");
+// } else {
+//     alert("Число не принадлежит интервалу");
+// }
+
+const user = {
+    name: "John",
+    pass: "ads2123"
+}
+
+const username = prompt("Введите имя пользователя", "User");
+const password = prompt("Введите пароль", "password");
+
+if (user.name == username && user.pass == password) {
+    alert("Привет");
+} else {
+    if (user.name == username && user.pass != password) {
+        alert("Неправильный пароль");
+    } else {
+        if (user.name != username && user.pass == password) {
+            alert("Неправильный username");
+        } else {
+            alert("Ошибка Валидации");
+        }
+    }
+}
+
+const username = prompt("Введите имя пользователя", "User");
+
+alert(`Привет ${username}`);
+
+const result = confirm("Вы знакомы с JavaScript");
+if (result == true) {
+    alert("Молодец");
+} else {
+    alert("Можно начать изучение прямо сейчас");
+}
