be85f97cd873afc542eb0952aaf3d63f2a64bec6
AnderRester <AnderRester@icloud.com>
2022-02-09 14:37:59 +0200

    Functions fix

--- JavaScript/tasks_chapter_1.js
+++ JavaScript/tasks_chapter_1.js
@@ -1,45 +1,45 @@
-// First task
-console.log("Hello, world!");
+// // First task
+// console.log("Hello, world!");
 
-// Third task
-//'use strict';
+// // Third task
+// //'use strict';
 
-// Fourth task
-let userName = "Student";
-console.log(userName);
-// You should never proceed like this!
-const adminName = "Admin";
-console.log(adminName);
+// // Fourth task
+// let userName = "Student";
+// console.log(userName);
+// // You should never proceed like this!
+// const adminName = "Admin";
+// console.log(adminName);
 
-// Fifth task
-let whaaaat;
-let truth = true;
-let number = 15;
-let line = "something";
-let objectExample = {};
+// // Fifth task
+// let whaaaat;
+// let truth = true;
+// let number = 15;
+// let line = "something";
+// let objectExample = {};
 
-console.log(whaaaat);
-console.log(truth);
-console.log(number);
-console.log(line);
-console.log(objectExample);
+// console.log(whaaaat);
+// console.log(truth);
+// console.log(number);
+// console.log(line);
+// console.log(objectExample);
 
-// Sixth task
+// // Sixth task
 
-//Small easter egg a saw films
-alert("I wanna play a game...");
-let answer = prompt("Unfortunately you have only 2 hours to death ", "Nooooo!");
-answer == "Nooooo!" ? alert("If you don't locate the key in time, the mask will close") : alert("So listen carefully...");
-confirm("Well, you'll die anyway...");
+// //Small easter egg a saw films
+// alert("I wanna play a game...");
+// let answer = prompt("Unfortunately you have only 2 hours to death ", "Nooooo!");
+// answer == "Nooooo!" ? alert("If you don't locate the key in time, the mask will close") : alert("So listen carefully...");
+// confirm("Well, you'll die anyway...");
 
-// Seventh task
-console.log(typeof whaaaat);
-console.log(typeof truth);
-console.log(typeof number);
-console.log(typeof line);
-console.log(typeof objectExample);
+// // Seventh task
+// console.log(typeof whaaaat);
+// console.log(typeof truth);
+// console.log(typeof number);
+// console.log(typeof line);
+// console.log(typeof objectExample);
 
-// The end of theoretical part
+// // The end of theoretical part
 
 
 //          Choosing the task controller                //
@@ -178,10 +178,10 @@ const chooseTaskCh1 = () => {
                 name: "John",
                 pass: "ads2123"
             }
-            
+
             const usernameBase = prompt("Введите имя пользователя", "User");
             const passwordBase = prompt("Введите пароль", "password");
-            
+
             if (user.name == usernameBase && user.pass == passwordBase) {
                 alert("Привет");
             } else {
--- JavaScript/tasks_chapter_2.js
+++ JavaScript/tasks_chapter_2.js
@@ -3,106 +3,130 @@
 const array = [1, 2, 3, 4, 6, 0];
 
 const chooseTaskCh2 = () => {
-    let option = document.getElementById("variant-2").value;
+    let option = document.getElementById("variant_2").value;
     switch (Number(option)) {
+        case 0:
+            const chooseSubFunction = +prompt("Введите номер желаемой подфункции (0-4)", 0);
+            switch (Number(option)) {
+                case 0:
+                    for (let i = 0; i < array.length; i++) {
+                        alert(array[i]);
+                    }
+                    break;
+                case 1:
+                    for (let i in array) {
+                        alert(array[i]);
+                    }
+                    break;
+                case 2:
+                    for (let i of array) {
+                        alert(i);
+                    }
+                    break;
+                case 3:
+                    array.forEach(i => alert(i));
+                    break;
+                case 4:
+                    let i = array.map((i) => alert(i));
+                    // let i = array.map(function(num) {
+                    //     alert(num);
+                    // });
+                    break;
+                }
+            break;
         case 1:
-            for (let i = 0; i < array.length; i++) {
-                alert(array[i]);
+            //                      Task 2.2                               //
+
+            const dataBase = [
+                first = {
+                    name: "First",
+                    marks: [2, 2, 7, 5, 2]
+                },
+                second = {
+                    name: "Second",
+                    marks: [8, 10, 4, 5, 4]
+                },
+                third = {
+                    name: "Third",
+                    marks: [8, 9, 7, 5, 6]
+                },
+                firth = {
+                    name: "Firth",
+                    marks: [8, 10, 7, 5, 5]
+                },
+                fifth = {
+                    name: "Fifth",
+                    marks: [8, 10, 9, 10, 10]
+                }
+            ]
+            let averageMarks = []
+
+            // for (let i = 0; i < dataBase.length; i++) {
+            //     alert(dataBase[i].marks.length);
+            // }
+
+
+            // for (let i = 0; i < dataBase.length; i++) {
+            //     unitAverage = Math.Average(dataBase[i].marks.length);
+            //     alert(1);
+            // }
+
+            let unitAverage = 1;
+            const average = () => {
+                    for (let i = 0; i < dataBase.length; i++) {
+                        for (let a = 0; a < dataBase[i].marks.length; a++) {
+                            unitAverage += dataBase[i].marks[a];
+                        }
+                        averageMarks[i] = unitAverage / dataBase[i].marks.length;
+                        return (averageMarks[i]);
+                        unitAverage = 0;
+                    }
+                }
+                // average(unitAverage);
+
+            const averageRemastered = () => {
+            console.log("Hello, there");
             }
+            averageRemastered();
+
+            const sort = () => {
+                for (let i = 0; i < dataBase.length; i++) {
+                    if (averageMarks[i] < 5) {
+                        alert(dataBase[i].name);
+                    }
+                }
+            }
+            sort();
+
+            // const sort = () => {
+            //     if (unitAverage < 5) {
+            //         alert(1);
+            //     }
+            // }
             break;
         case 2:
-            for (let i in array) {
-                alert(array[i]);
-            }
+            //                      Task 2.3                               //
             break;
         case 3:
-            for (let i of array) {
-                alert(i);
-            }
+            alert(1);
             break;
         case 4:
-            array.forEach(i => alert(i));
+            alert(1);
             break;
         case 5:
-            let i = array.map((i) => { alert(i) });
-            // let i = array.map(function(num) {
-            //     alert(num);
-            // });
+            alert(1);
             break;
         case 6:
             alert(1);
             break;
+        case 7:
+            alert(1);
+            break;
+        case 8:
+            alert(1);
+            break;
+        case 9:
+            alert(1);
+            break;
     }
 }
-
-
-//                      Task 2.2                               //
-
-
-const dataBase = [
-    first = {
-        name: "First",
-        marks: [2, 2, 7, 5, 2]
-    },
-    second = {
-        name: "Second",
-        marks: [8, 10, 4, 5, 4]
-    },
-    third = {
-        name: "Third",
-        marks: [8, 9, 7, 5, 6]
-    },
-    firth = {
-        name: "Firth",
-        marks: [8, 10, 7, 5, 5]
-    },
-    fifth = {
-        name: "Fifth",
-        marks: [8, 10, 9, 10, 10]
-    }
-]
-let averageMarks = []
-
-// for (let i = 0; i < dataBase.length; i++) {
-//     alert(dataBase[i].marks.length);
-// }
-
-
-// for (let i = 0; i < dataBase.length; i++) {
-//     unitAverage = Math.Average(dataBase[i].marks.length);
-//     alert(1);
-// }
-
-let unitAverage = 1;
-const average = () => {
-        for (let i = 0; i < dataBase.length; i++) {
-            for (let a = 0; a < dataBase[i].marks.length; a++) {
-                unitAverage += dataBase[i].marks[a];
-            }
-            averageMarks[i] = unitAverage / dataBase[i].marks.length;
-            return (averageMarks[i]);
-            unitAverage = 0;
-        }
-    }
-    // average(unitAverage);
-    //average(unitAverage);
-
-
-const sort = () => {
-    for (let i = 0; i < dataBase.length; i++) {
-        if (averageMarks[i] < 5) {
-            alert(dataBase[i].name);
-        }
-    }
-}
-
-sort();
-
-// const sort = () => {
-//     if (unitAverage < 5) {
-//         alert(1);
-//     }
-// }
-
-
-//                      Task 2.3                               //
